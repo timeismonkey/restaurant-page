6ae5038e5aba93935bc7b2f8261381ee71fc7765
Jerry/Laptop <jerrykeiko98@gmail.com>
2024-03-18 15:05:31 -0700

    Added contact-page-load.js and contact button logic go index.js.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
 import initial from './initial-page-load';
 import menu from './menu-page-load';
+import contact from './contact-page-load';
 
 const homeBtn = document.querySelector('#home-btn');
 const menuBtn = document.querySelector('#menu-btn');
-const aboutBtn = document.querySelector('#about-btn');
+const contactBtn = document.querySelector('#contact-btn');
 const contentContainer = document.querySelector('#content');
 
 // Load initial page content
@@ -21,3 +22,8 @@ menuBtn.addEventListener('click', () => {
     clearContainer();
     menu(contentContainer);
 });
+
+contactBtn.addEventListener('click', () => {
+    clearContainer();
+    contact(contentContainer);
+})
