bbba990ef745a9b146bdff89049ced4ab59730f0
Jerry/Laptop <jerrykeiko98@gmail.com>
2024-03-17 17:10:40 -0700

    Added homeBtn event listener.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import initial from './initial-page-load';
 const homeBtn = document.querySelector('#home-btn');
 const menuBtn = document.querySelector('#menu-btn');
 const aboutBtn = document.querySelector('#about-btn');
-
 const contentContainer = document.querySelector('#content');
 
 // Load initial page content
 initial(contentContainer);
 
+const clearContainer = () => contentContainer.innerHTML = '';
+
 homeBtn.addEventListener('click', () => {
-    
+    clearContainer();
     initial(contentContainer)
-})
+});
+
--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -6,13 +6,15 @@ export default function initial(container) {
     const about = document.createElement('p');
 
     img.src = Restaurant;
-    img.alt = 'Restaurant dining area with lights hanging and white tablecloths';
-    headline.innerHTML = "Our Restaurant";
-    about.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia minus, deleniti dolorem officia ex sequi expedita commodi architecto distinctio, quisquam perspiciatis voluptates dolor officiis temporibus doloremque hic cupiditate molestias reiciendis.";
+    img.alt =
+        'Restaurant dining area with lights hanging and white tablecloths';
+    headline.innerHTML = 'Our Restaurant';
+    about.innerHTML =
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia minus, deleniti dolorem officia ex sequi expedita commodi architecto distinctio, quisquam perspiciatis voluptates dolor officiis temporibus doloremque hic cupiditate molestias reiciendis.';
 
-    Container.appendChild(img);
-    Container.appendChild(headline);
-    Container.appendChild(about);
+    container.appendChild(img);
+    container.appendChild(headline);
+    container.appendChild(about);
 
     img.classList.add('main-page-img');
     headline.classList.add('headline');
